2c0b2c7a48b8a331574da74cedefd1448bdd5237
Ubuntu <ubuntu@ip-172-31-16-18.us-west-2.compute.internal>
2020-04-10 07:07:34 +0000

    puhing the script's latest changes

--- privia_automation.sh
+++ privia_automation.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #function that disables usb
 function usb_disable ()
@@ -18,33 +18,35 @@ function usb_disable ()
 function firewall_enable ()
 {
 	#case:1 --installing firewall if not installed
-	uninstalled=$(sudo dpkg --get-selections | grep ufw)
-	if [ -z "$uninstalled" ]
-	then
-		sudo apt install ufw -y	#install ufw firewall
+        uninstalled=$(sudo dpkg --get-selections | grep ufw)
+        if [ -z "$uninstalled" ]
+        then
+                sudo apt install ufw -y #install ufw firewall
 
-	#case:2 --making firewall active if inactive
-	else 
-		status=$(sudo ufw status | head -1 | cut -d : -f 2) #storing the status of firewall
-		if [ $status = 'inactive' ]
-		then
-			sudo ufw reset		#deleting all the previous rules
-		fi
-	fi
+        #case:2 --making firewall active if inactive
+        else
+                status=$(sudo ufw status | head -1 | cut -d : -f 2) #storing the status of firewall
+                if [ $status = 'inactive' ]
+                then
+                        echo y | sudo ufw reset          #deleting all the previous rules
+                fi
+        fi
 
-	#case:3 --when firewall is active an if ports other than 53 and 22 are open. 
-	open_ports=$(sudo ufw status | awk '{if(NR>4)print}' | cut -d " " -f 1 | grep -v 22 | grep -v 53)
-	if [ -z "$open_ports" ]
-		#do nothing if no ports are open other than 22 and 53
-	then
-	else
-		#resetting the rules if any port other than 53 and 22 are open
-		sudo ufw reset		#deleting all the rules
-	fi
-	
-	sudo ufw allow 53 #allows port 53
-	sudo ufw allow 22 #allows port 22
-	sudo ufw enable   #enabling the firewall
+        #case:3 --when firewall is active an if ports other than 53 and 22 are open.
+        open_ports=$(sudo ufw status | awk '{if(NR>4)print}' | cut -d " " -f 1 | grep -v 22 | grep -v 53)
+        if [ -z "$open_ports" ]
+        then
+                #do nothing if no ports are open other than 22 and 53
+                echo "no ports other than 22 and 53 are open"
+        else
+                #resetting the rules if any port other than 53 and 22 are open
+                echo y | sudo ufw reset          #deleting all the rules
+        fi
+
+        sudo ufw allow 53 #allows port 53
+        sudo ufw allow 22 #allows port 22
+        echo y | sudo ufw enable #enabling the firewall
+
 }
 
 
@@ -61,6 +63,7 @@ function package_update()
 function disable_print_screen ()
 {
 	#sudo chmod -x /usr/bin/gnome-screenshot
+	echo "doing nothing as of now"
 }
 
 
